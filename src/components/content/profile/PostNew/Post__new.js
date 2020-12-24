d2ee707ccecda460daeff30b0b76c938a09445d0
User <user@MacBook-Pro-User.local>
2020-12-24 23:46:49 +0300

    added onclick and refs for dialogs and profile

--- src/components/content/profile/PostNew/Post__new.js
+++ src/components/content/profile/PostNew/Post__new.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import classes from './post__new.module.css';
 
+let newPostElemet = React.createRef();
+
+let addPost = () => {
+   let newPost = newPostElemet.current.value;
+   alert(newPost)
+}
+
 const Post__new = () => {
    return(
       <div className = {classes.content__new}>
-         <textarea placeholder = "Write something and post it!"></textarea>
-         <button className={classes.content__button}>Post it</button>
+         <textarea ref = {newPostElemet}  placeholder = "Write something and post it!"></textarea>
+         <button onClick = {addPost} className={classes.content__button}>Post it</button>
       </div>
    )
 }
--- src/components/dialogs/Messages/CreateMessage.js
+++ src/components/dialogs/Messages/CreateMessage.js
@@ -2,10 +2,17 @@ import React from 'react';
 import './createMessage.css';
 
 const CreateMessage = () => {
+   let newMessage = React.createRef();
+
+   let createMessage = () => {
+      let message = newMessage.current.value;
+      alert (message);
+   }
+
    return (
       <div className = 'new__message'>
-         <textarea placeholder = 'Write your message here'/>
-         <button>Submit</button>
+         <textarea ref = {newMessage} placeholder = 'Write your message here'/>
+         <button onClick = {createMessage}>Submit</button>
       </div>
    )
 }
